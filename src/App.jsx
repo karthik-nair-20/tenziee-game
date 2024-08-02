9becc8f99f76dd0ec893e9117388b77083bee50c
kxrthiknair <herbolax77@gmail.com>
2024-08-02 13:26:11 +0530

    made fix till dice can get be held

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
 import React from "react";
 import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
+  // thie is an array of object
+  const [dice, setDice] = React.useState(allNewDice())
 
-  const [array, setArray] = React.useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-  const dieElement = array.map((index, value) => (
-    <Die key={index} index={index} value={value} />
-  ));
 
-  // i want the values of array to change
-  function handleClick() {
-    const newArray = [...array]
-    for(let i=newArray.length - 1; i>0;i--) {
-      const j = Math.floor(Math.random() * (i+1))
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  function allNewDice() {
+    let newDice = []
+    for(let i=0;i<10;i++)
+    {
+      newDice.push(generateDice())
     }
-    setArray(newArray)
-    console.log(setArray)
+    return newDice
   }
+
+  function generateDice() {
+    return {
+      value: getRandomVal(),
+      isHeld: false,
+      id:nanoid()
+    }
+  }
+
+  function getRandomVal() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  function onHold(id) {
+    // is held to false on this particular ID
+    setDice((prevDice) => {
+      return prevDice.map((die) => (
+          die.id === id ? {...die, isHeld : !die.isHeld} : die
+        ))
+    })
+  }
+
+  const diceElement = dice.map((ele) =>  {
+    return (
+      <Die 
+        key ={ele.id}
+        value ={ele.value}
+        isHeld={ele.isHeld}
+        onSelectDie ={() => onHold(ele.id)}
+      />
+    )}
+  )
 
   return (
     <div className="bg-slate-700 w-screen h-screen m-0 p-0">
       <div className="grid grid-cols-5 grid-rows-2 gap-2 bg-slate-50 mx-32 p-8 my-auto w-1/2">
-        {dieElement}
-        <button className="bg-blue-500 w-full h-full p-2 text-white text-lg rounded-lg hover:bg-blue-600"
-          onClick={handleClick}
-        >
+        {diceElement}
+        <button className="bg-blue-500 w-full h-full p-2 text-white text-lg rounded-lg hover:bg-blue-600">
           Roll
         </button>
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Die({value}) {
+export default function Die(props) {
   return (
-      <div className="flex justify-center items-center border-6 bg-white w-9 p-3 m-2 shadow-[0px_2px_2px_rgba(0,0,0,0.15)] cursor-pointer rounded-lg">
-        <h2 className="text-2xl">{value}</h2>
+      <div className= {`flex justify-center items-center border-4 bg-white w-9 p-3 m-2 shadow-[0px_2px_2px_rgba(0,0,0,0.15)] cursor-pointer rounded-lg ${props.isHeld ? 'bg-green-500' : 'bg-white' }`} onClick={props.onSelectDie}>
+        <h2 className="text-2xl">{props.value}</h2>
       </div>
-  )
+  ) 
 }
